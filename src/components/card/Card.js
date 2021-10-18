b5a753e47b9b6c6314f320a6b6e1ad133b901a6b
mehmetasilkilic <mehmetasil96@gmail.com>
2021-10-18 14:37:06 +0300

    a

---
+++ src/components/card/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './card.scss'
+
+const Card = () => {
+    return (
+        <div className="card">
+            
+        </div>
+    )
+}
+
+export default Card
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navbar.scss'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="wrapper">
+                <h1>Shopping Cart</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
